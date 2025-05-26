7780e13751b77b6a7138e35992347694c142f7fe
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-05-26 20:27:37 -0300

    Enviando prjeto ja finalizado

--- src/introducaoPoo/entities/Triangle.java
+++ src/introducaoPoo/entities/Triangle.java
@@ -3,5 +3,11 @@ package introducaoPoo.entities;
 public class Triangle {
     public double a;
     public double b;
-    public double c;
+    public double c;git
+
+    public double area (){
+       double p = (a + b + c) / 2.0;
+       double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+       return result;
+    }
 }
