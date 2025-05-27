14f9ff0a6861854c0c7413da38404c6c38bacc42
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-05-27 19:08:38 -0300

    Exercicio de Poo 1

--- src/introducaoPoo/application/MainRectangle.java
+++ src/introducaoPoo/application/MainRectangle.java
@@ -1,4 +1,25 @@
 package introducaoPoo.application;
 
+import introducaoPoo.entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MainRectangle {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Rectangle rectangle = new Rectangle();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rectangler width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("Area: "+rectangle.Area());
+        System.out.println("Perimeter: "+rectangle.perimeter());
+        System.out.println("Diagonal: "+rectangle.diagonal());
+
+
+
+    }
 }
--- src/introducaoPoo/entities/Rectangle.java
+++ src/introducaoPoo/entities/Rectangle.java
@@ -1,4 +1,16 @@
 package introducaoPoo.entities;
 
 public class Rectangle {
+    public double width;
+    public double height;
+
+    public double Area(){
+        return width * height;
+    }
+    public double perimeter(){
+        return 2*width+2*height;
+    }
+    public double diagonal(){
+        return  Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+    }
 }
