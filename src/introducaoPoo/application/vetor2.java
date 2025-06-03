cbee613d1f10cd53a9e6b229170114932403e21d
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-06-03 19:53:41 -0300

    Exercicio fixação vetores

--- src/introducaoPoo/application/vetor2.java
+++ src/introducaoPoo/application/vetor2.java
@@ -1,4 +1,37 @@
 package introducaoPoo.application;
 
+import introducaoPoo.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class vetor2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+        for(int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / n;
+        System.out.println("Average Price: "+ avg);
+
+
+
+
+
+
+
+
+    }
 }
