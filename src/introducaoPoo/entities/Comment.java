bb2421df2dd2f17c03bb31fe466f90177f66857e
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-07-16 20:29:00 -0300

    Aprendendo toStringBuider

---
+++ src/introducaoPoo/entities/Comment.java
@@ -0,0 +1,21 @@
+package introducaoPoo.entities;
+
+public class Comment {
+  private String text;
+
+  public Comment(){
+
+  }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
